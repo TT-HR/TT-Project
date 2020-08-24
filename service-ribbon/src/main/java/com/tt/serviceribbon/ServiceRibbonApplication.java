5fad7ddfd74963e470dd042540046246f51b8613
TTHR <yhr199792@gmail.com>
2020-08-24 23:56:24 +0800

    ribbon

--- service-ribbon/src/main/java/com/tt/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/tt/serviceribbon/ServiceRibbonApplication.java
@@ -2,10 +2,17 @@ package com.tt.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author TT
  */
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceRibbonApplication {
 
@@ -13,4 +20,9 @@ public class ServiceRibbonApplication {
         SpringApplication.run(ServiceRibbonApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
