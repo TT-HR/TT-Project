a02302dbc17fa942a1dde97e2db28de9c7c8f0fc
TTHR <yhr199792@gmail.com>
2020-08-24 23:43:03 +0800

    框架搭建

---
+++ eureka-server/src/main/java/com/tt/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.tt.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author TT
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ service-ribbon/src/main/java/com/tt/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,16 @@
+package com.tt.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TT
+ */
+@SpringBootApplication
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+}
